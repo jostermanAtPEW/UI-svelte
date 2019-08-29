cef5ff6aa8b01e72b954d7099d21c6ca0ab83532
John Osterman <josterman@pewtrusts.org>
2019-08-29 11:13:42 -0400

    Positions x-out image to right

--- x-out/index.svelte
+++ x-out/index.svelte
@@ -4,7 +4,7 @@
 
 <style>
     .x-out {
-        background: url("data:image/svg+xml;charset=utf-8,%3Csvg width='16' height='14' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M8 7l-6.6 6.6L8 7 1.4.4 8 7zm0 0L14.7.3 8 7l6.6 6.6L8 7z' stroke-linecap='round' stroke-linejoin='round' stroke='%23296EC3'/%3E%3C/svg%3E") no-repeat;
+        background: url("data:image/svg+xml;charset=utf-8,%3Csvg width='16' height='14' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M8 7l-6.6 6.6L8 7 1.4.4 8 7zm0 0L14.7.3 8 7l6.6 6.6L8 7z' stroke-linecap='round' stroke-linejoin='round' stroke='%23296EC3'/%3E%3C/svg%3E") no-repeat right;
         width: 23px;
         height: 30px;
         border: none;
